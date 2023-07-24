23c877549639a2e9df45e4e5183ac2e54614a554
Shrut Shah <43044788+ShrutShah@users.noreply.github.com>
2023-07-24 13:34:26 -0500

    Create for-of, for-in.js

---
+++ for-of, for-in.js
@@ -0,0 +1,16 @@
+let arr = Array.from("html/object/collection")
+
+//convert other object to array so that we can use forEach and all.
+
+console.log(arr)
+
+
+for(i of arr){
+    
+    console.log(i)
+} 
+
+for(i in arr){
+    
+    console.log(i)
+} 
