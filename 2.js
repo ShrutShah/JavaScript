21a4645fae2981bbeda511df886c47cbb4797177
Shrut Shah <43044788+ShrutShah@users.noreply.github.com>
2023-07-24 09:28:19 -0500

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,21 @@
+let a = "str"
+a = 3
+
+console.log(typeof(a));
+
+
+
+
+const b = {
+    a: "shrut",
+    b:12 
+}
+
+b["a"] = "namita"
+console.log(b)
+
+console.log(b.a)
+
+let x = 1
+let y = 2
+console.log("x+y =",x+y)
