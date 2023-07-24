bf114b047205518f48d4eb76c5ded5da00c7487a
Shrut Shah <43044788+ShrutShah@users.noreply.github.com>
2023-07-24 09:27:10 -0500

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,15 @@
+let a = null;
+let b = 12;
+
+let c = true;
+let d = "str";
+
+let e = BigInt("12");
+let g = Symbol("hi I'm shrut ")
+
+item = {
+    a:"shrut",
+    b:true 
+}
+console.log(item['a'])
+
