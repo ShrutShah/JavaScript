8f96f1f87877ecf4de6c354bd99e69d720823bce
Shrut Shah <43044788+ShrutShah@users.noreply.github.com>
2023-07-24 12:28:01 -0500

    Create 9.js

---
+++ 9.js
@@ -0,0 +1,30 @@
+let arr = [1,2,"shrut",4]
+
+console.log(arr[2])
+
+console.log(arr.length)
+
+console.log(arr.toString())
+
+console.log(arr.join("-"))
+
+console.log(arr.pop())
+console.log(arr)
+
+console.log(arr.push(1))
+console.log(arr)
+
+//pop - from end & shift from start
+//push - add at the end & unshift add at start.
+
+
+// delete will del but it won't alter the array length.
+delete(arr[0])
+console.log(arr)
+
+num2 =[2,3]
+
+console.log(arr.concat(num2))
+
+console.log(arr.sort())
+
