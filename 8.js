30d4b7aa9861799e91201053a9833156564db5db
Shrut Shah <43044788+ShrutShah@users.noreply.github.com>
2023-07-24 10:25:23 -0500

    Create 8.js

---
+++ 8.js
@@ -0,0 +1,18 @@
+let name = `shRut`
+
+console.log(name.length)
+
+console.log(name.toUpperCase())
+
+console.log(name.toLowerCase())
+
+console.log(name.slice(1,3))
+
+console.log(name.replace("R","r"))
+name2 = "akshay"
+
+console.log(name.concat(" is a friend of ",name2))
+
+name3 = "         shrut      "
+
+console.log(name3.trim())
